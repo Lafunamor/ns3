ce9def3b66af4433c80f7529e2071f27891604a4
Lafunamor <info@zipfuzapfu.ch>
2014-04-10 10:20:57 +0200

    lte test file

---
+++ scratch/lte_ex.cc
@@ -0,0 +1,37 @@
+#include <ns3/core-module.h>
+#include <ns3/network-module.h>
+#include <ns3/mobility-module.h>
+#include <ns3/lte-module.h>
+
+using namespace ns3;
+
+int main (int argc, char *argv[]){
+
+Ptr<Helper> lteHelper = CreateObject<LteHelper>();
+
+NodeContainer enbNodes;
+enbNodes.Create (1);
+NodeConationer ueNodes;
+ueNodes.Create(2);
+
+MobilityHelper mobility;
+mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+mobility.Install (enbNodes);
+mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+mobility.Install (ueNodes);
+
+NetDeviceContainer enbDevs;
+enbDevs = lteHelper->InstallEnbDevice (enbNodes);
+
+lteHelper->Attach (ueDevs, enbDevs.Get (0));
+
+enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+EpsBearer bearer (q);
+lteHelper->ActivateDataRadioBearer (ueDevs, bearer);
+
+Simulator::Stop (Seconds (0.005));
+Simulator::Run ();
+
+Simulator::Destroy ();
+return 0;
+}
