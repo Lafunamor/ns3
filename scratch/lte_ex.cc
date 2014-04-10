400a2fa4aa80b5f8f064100e4e9eec1473eaa07c
Lafunamor <info@zipfuzapfu.ch>
2014-04-10 11:02:36 +0200

    added cfg file and command.txt

--- scratch/lte_ex.cc
+++ scratch/lte_ex.cc
@@ -1,4 +1,7 @@
+/* */
+
 #include <ns3/core-module.h>
+#include <ns3/config-store.h>
 #include <ns3/network-module.h>
 #include <ns3/mobility-module.h>
 #include <ns3/lte-module.h>
@@ -7,6 +10,13 @@ using namespace ns3;
 
 int main (int argc, char *argv[]){
 
+CommandLine cmd;
+cmd.Parse (argc, argv);
+ConfigStore inputConfig;
+inputConfig.ConfigureDefaults ();
+// parse again so you can override default values from the command line
+cmd.Parse (argc, argv);    
+    
 Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
 
 NodeContainer enbNodes;
@@ -32,6 +42,5 @@ lteHelper->ActivateDataRadioBearer (ueDevs, bearer);
 
 Simulator::Stop (Seconds (0.005));
 Simulator::Run ();
-
 
 }
