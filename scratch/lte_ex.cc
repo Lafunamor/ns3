acd4afc5d4f3ef50313efd894a4f1b447be253d8
Lafunamor <info@zipfuzapfu.ch>
2014-04-24 14:21:45 +0200

    added some comments and output generation

--- scratch/lte_ex.cc
+++ scratch/lte_ex.cc
@@ -8,39 +8,74 @@
 
 using namespace ns3;
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-CommandLine cmd;
-cmd.Parse (argc, argv);
-ConfigStore inputConfig;
-inputConfig.ConfigureDefaults ();
-// parse again so you can override default values from the command line
-cmd.Parse (argc, argv);    
-    
-Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+    CommandLine cmd;
+    cmd.Parse(argc, argv);
+    ConfigStore inputConfig;
+    inputConfig.ConfigureDefaults();
+    // parse again so you can override default values from the command line
+    cmd.Parse(argc, argv);
 
-NodeContainer enbNodes;
-enbNodes.Create (1);
-NodeContainer ueNodes;
-ueNodes.Create (2);
+    Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
 
-MobilityHelper mobility;
-mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-mobility.Install (enbNodes);
-mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-mobility.Install (ueNodes);
+    /* set scheduler
+     * 
+     * lteHelper->SetSchedulerType ("ns3::FdMtFfMacScheduler");    // FD-MT scheduler
+     * lteHelper->SetSchedulerType ("ns3::TdMtFfMacScheduler");    // TD-MT scheduler
+     * lteHelper->SetSchedulerType ("ns3::TtaFfMacScheduler");     // TTA scheduler
+     * lteHelper->SetSchedulerType ("ns3::FdBetFfMacScheduler");   // FD-BET scheduler
+     * lteHelper->SetSchedulerType ("ns3::TdBetFfMacScheduler");   // TD-BET scheduler
+     * lteHelper->SetSchedulerType ("ns3::FdTbfqFfMacScheduler");  // FD-TBFQ scheduler
+     * lteHelper->SetSchedulerType ("ns3::TdTbfqFfMacScheduler");  // TD-TBFQ scheduler
+     * lteHelper->SetSchedulerType ("ns3::PssFfMacScheduler");     //PSS scheduler
+     * 
+     * * TBFQ scheduler::
+     * 
+     * Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+     * lteHelper->SetSchedulerAttribute("DebtLimit", IntegerValue(yourvalue)); // default value -625000 bytes (-5Mb)
+     * lteHelper->SetSchedulerAttribute("CreditLimit", UintegerValue(yourvalue)); // default value 625000 bytes (5Mb)
+     * lteHelper->SetSchedulerAttribute("TokenPoolSize", UintegerValue(yourvalue)); // default value 1 byte
+     * lteHelper->SetSchedulerAttribute("CreditableThreshold", UintegerValue(yourvalue)); // default value 0
 
-NetDeviceContainer enbDevs;
-enbDevs = lteHelper->InstallEnbDevice (enbNodes);
+     * PSS scheduler::
 
-NetDeviceContainer ueDevs;
-lteHelper->Attach (ueDevs, enbDevs.Get (0));
+     * Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+     * lteHelper->SetSchedulerAttribute("nMux", UIntegerValue(yourvalue)); // the maximum number of UE selected by TD scheduler
+     * lteHelper->SetSchedulerAttribute("PssFdSchedulerType", StringValue("CoItA")); // PF scheduler type in PSS
+     */
+    lteHelper->SetSchedulerType("ns3::FdMtFfMacScheduler"); // FD-MT scheduler
 
-enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
-EpsBearer bearer (q);
-lteHelper->ActivateDataRadioBearer (ueDevs, bearer);
+    NodeContainer enbNodes;
+    enbNodes.Create(1);
+    NodeContainer ueNodes;
+    ueNodes.Create(2);
 
-Simulator::Stop (Seconds (0.005));
-Simulator::Run ();
+    MobilityHelper mobility;
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(enbNodes);
+    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    mobility.Install(ueNodes);
+
+    NetDeviceContainer enbDevs;
+    enbDevs = lteHelper->InstallEnbDevice(enbNodes);
+
+    NetDeviceContainer ueDevs;
+    lteHelper->Attach(ueDevs, enbDevs.Get(0));
+
+    enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+    EpsBearer bearer(q);
+    lteHelper->ActivateDataRadioBearer(ueDevs, bearer);
+
+    // configure all the simulation scenario here...
+
+    lteHelper->EnablePhyTraces();
+    lteHelper->EnableMacTraces();
+    lteHelper->EnableRlcTraces();
+    lteHelper->EnablePdcpTraces();
+
+
+    Simulator::Stop(Seconds(0.005));
+    Simulator::Run();
 
 }
